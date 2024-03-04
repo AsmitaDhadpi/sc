c1e94c61501e27c6da43122b5d522035be60b219
AsmitaDhadpi <123357808+AsmitaDhadpi@users.noreply.github.com>
2024-03-04 09:29:15 +0530

    Add files via upload

---
+++ Pract1_a_Net_input.py
@@ -0,0 +1,6 @@
+x=float(input("Enter the value of Input : "))
+w=float(input("Enter the value of Weight : "))
+b=float(input("Enter the Value of Bias : "))
+net =float((x*w)+b)
+
+print("Net Input (yin): ",net)
